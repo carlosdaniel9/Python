0b3d540065e51e05168083263ceef132cac5ad75
carlosdaniel9 <133067710+carlosdaniel9@users.noreply.github.com>
2023-09-09 10:15:31 -0300

    Create ex17- cateto e hipotenusa.py

---
+++ ex17- cateto e hipotenusa.py
@@ -0,0 +1,9 @@
+import math
+co = float(input('Qual a medida do cateto oposto?'))
+ca = float(input('Qual a medida do cateto adjacente?'))
+
+cal = (co ** 2 + ca ** 2) ** (1/2) # essa é a formula matematica 
+cal2 = math.hypot(co, ca)
+
+print('A soma entre o cateto oposto pelo adjacente, da uma hipotenusa de {:.2f}'.format(cal))
+print('a hipotenusa de cateto oposto {} e do adjacente {} é {:.2f}, usando hypot do math'.format(co, ca, cal2))
