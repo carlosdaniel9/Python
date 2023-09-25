5112b2d737d9f689f76f6b28e963d4f815d9e97f
Carlos Daniel <133067710+carlosdaniel9@users.noreply.github.com>
2023-09-25 23:41:26 +0000

    Update ex07-media entre 2 numeros.py

--- ex07-media entre 2 numeros.py
+++ ex07-media entre 2 numeros.py
@@ -4,4 +4,4 @@ media = (n1 + n2) / 2 # aqui esta somando os valores da variavel, de dividindo p
 # os () para n1+n2, para para primeiro fazer a soma das notas, ai so depois ele dividir por 2ß
 
 print('A média entre {:.1f}, e {:.1f} é {:.1f}'.format(n1, n2, media))
-# :.1f, está dizendo que depopis do ponto flutuante, coloque apenas 1 digite(casa decimas)
+# :.1f, está dizendo que depois do ponto flutuante, coloque apenas 1 digite(1 casa decimas)
