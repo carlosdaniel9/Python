e4d3ad48b1b669b2cac5a8803a7e123095b0239c
Carlos Daniel <djkarllostehbesthotmail.com@gmail.com>
2023-09-30 23:07:44 -0300

    add

--- aula 10 Condições (Parte 1).py
+++ aula 10 Condições (Parte 1).py
@@ -9,4 +9,6 @@ else:
 # método simplificado
 
 print('Carro novo'if tempo <=5 else'carro velho')
-# printa carro novo se tempo for menor ou igual a 5, senão printa carro velho.
+# printa carro novo se tempo for menor ou igual a 5, senão printa carro velho. 
+
+
